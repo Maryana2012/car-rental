6c07e2c767081cfd5db6d9446ac5dc00ca4e07c1
Maryana2012 <maryana.oryshchuk@gmail.com>
2023-09-17 17:26:44 +0300

    styled

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,12 +3,8 @@ import css from '../HomePage/HomePage.module.css'
 const HomePage = () => { 
     return (
         <main className={css.container}>
-            <h1>Car rental</h1>
-            <div>
-              <img className={css.imageFirst}  src="/images/car_rental.jpg" alt="car"/>
-
-            </div>
-          {/* <img className={css.imageFirst} src="/images/images.jpg" alt="car"/> */}
+            <h1>Cruise into Convenience</h1>
+            <p className={css.descriptionFirst } >Welcome to your trusted partner in hassle-free travel experiences. Discover a world of convenience and choice as you explore our extensive fleet of rental cars. Whether you're planning a weekend getaway, a family vacation, or a business trip, we've got the perfect vehicle to suit your needs.</p>
         </main>
     )
 }
