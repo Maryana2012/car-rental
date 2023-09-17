c7da110c235d0ba144ff84d95bea8302bed7149d
Maryana2012 <maryana.oryshchuk@gmail.com>
2023-09-17 15:34:18 +0300

    home

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,16 @@
-const HomePage = () => { 
+import css from '../HomePage/HomePage.module.css'
 
+const HomePage = () => { 
+    return (
+        <main className={css.container}>
+            <h1>Car rental</h1>
+            <div>
+              <img className={css.imageFirst}  src="/images/car_rental.jpg" alt="car"/>
+
+            </div>
+          {/* <img className={css.imageFirst} src="/images/images.jpg" alt="car"/> */}
+        </main>
+    )
 }
 
 export default HomePage;
